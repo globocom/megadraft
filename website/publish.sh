ebc110e9db8cc99decc1a85889e70af5aebb9e67
Travis CI <travis@globo.com>
2016-03-23 22:35:04 +0000

    Update github pages

--- website/publish.sh
+++ website/publish.sh
@@ -4,12 +4,14 @@ cd "$(dirname "$0")"
 cd ..
 npm run site
 git clone --branch gh-pages --depth=50 \
-        "https://vierno@github.com/globocom/megadraft.git"
+        "https://$GH_TOKEN@github.com/globocom/megadraft.git"
 rm -rf megadraft/website
 mv website megadraft
 cd megadraft
 rm index.html
 mv website/index.html index.html
 git add -A .
-git commit -m "Update github pages"
-git push origin gh-pages
+# if ! git diff-index --quiet HEAD --; then
+  git commit -m "Update github pages"
+  git push origin gh-pages
+# fi
