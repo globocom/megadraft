1ffa827b98008aa30b5518a0e7f5f4567250719b
Guilherme Vierno <guilhermevierno@gmail.com>
2016-03-23 17:09:36 -0300

    Trying to fix the page publish script (again)

--- website/publish.sh
+++ website/publish.sh
@@ -5,7 +5,9 @@ cd ..
 npm run site
 mv website website_new
 git fetch origin gh-pages
-git checkout gh-pages
+git checkout origin/gh-pages
+git checkout -b gh-pages
+git branch --set-upstream-to=origin/gh-pages
 rm -rf website
 rm index.html
 mv website_new website
