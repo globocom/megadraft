e7da09d8ad43dcb536f1ea062f0b994efd624465
Guilherme Vierno <guilhermevierno@gmail.com>
2016-03-23 11:29:21 -0300

    Updates website with navigation

---
+++ website/components/docs.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Overview from '../../docs/overview.md';
+
+
+const DOCS = {
+  overview: Overview
+}
+
+
+export default class Docs extends React.Component {
+  render() {
+    const {doc} = this.props.params
+    return (
+      <div dangerouslySetInnerHTML={{__html: DOCS[doc]}}></div>
+    )
+  }
+}
