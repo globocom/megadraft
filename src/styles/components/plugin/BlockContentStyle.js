dd5d37fed90d432c072bea5777acfc2574e3353e
Enrico <rico.bl@gmail.com>
2016-06-06 19:08:17 -0300

    Added lines on image block to separate image from controls

--- src/styles/components/plugin/BlockContentStyle.js
+++ src/styles/components/plugin/BlockContentStyle.js
@@ -6,7 +6,9 @@
 
 export default {
   wrapper: {
-    backgroundColor: "#eee"
+    backgroundColor: "#eee",
+    borderTop: "1px solid rgba(0, 0, 0, 0.1)",
+    borderBottom: "1px solid rgba(0, 0, 0, 0.1)"
   },
   imageWrapper: {
     lineHeight: "100px",
