32a748d5557640ee9f97b92d06523ca670eaafb9
Guilherme Vierno <guilhermevierno@gmail.com>
2016-06-10 14:54:35 -0300

    Remove Radium dependency

--- src/icons/dropdownArrow.js
+++ src/icons/dropdownArrow.js
@@ -5,11 +5,9 @@
  */
 
 import React from "react";
-import Radium from "radium";
 
 
-export default @Radium
-class extends React.Component {
+export default class extends React.Component {
   render() {
     return (
         <svg {...this.props} width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
