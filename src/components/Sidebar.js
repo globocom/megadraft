74cc25507aba6868e359999c046f9d4124e5b508
Manuel Darcemont <manuel.darcemont@gmail.com>
2016-06-17 10:14:20 +0200

    Import YuzuJS/setimmediate for setImmediate / clearImmediate browser compatibility

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,6 +8,8 @@ import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import icons from "../icons";
 
+import 'setimmediate';
+
 
 class BlockStyles extends Component {
   render() {
