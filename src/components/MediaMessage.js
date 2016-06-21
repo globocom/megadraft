0adf97087acd80667acbd5265ccf7d7585e6004d
Marcelo Jorge Vieira <metal@alucinados.com>
2016-06-21 13:40:16 -0300

    Initial version of Media Message

---
+++ src/components/MediaMessage.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2016, Globo.com (https://github.com/globocom)
+ *
+ * License: MIT
+ */
+
+import React, {Component} from "react";
+import classNames from "classnames";
+
+
+export default
+class MediaMessage extends Component {
+  render() {
+    if (this.props.text) {
+      const error_type = (this.props.type) ? this.props.type : "info";
+      const messageClassName = classNames(`media__message media__message--${error_type}`);
+      return (
+        <div className={messageClassName}>
+          <div className="media__message-text">{this.props.text}</div>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+}
