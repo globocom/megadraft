d0dbcbb674319b580f87a56a5cda8f1c7bd5abbd
Enrico <rico.bl@gmail.com>
2016-07-28 18:43:18 -0300

    Renamed `featured` option of blocks to `display`

--- tests/components/plugin/CommonBlock_test.js
+++ tests/components/plugin/CommonBlock_test.js
@@ -23,12 +23,12 @@ describe("CommonBlock Component", function() {
     this.crop = sinon.spy();
 
     const actions = [{key: "crop", icon: icons.CropIcon, action: this.crop}];
-    const featuredOptions = [
+    const displayOptions = [
       {key: "small", icon: icons.MediaSmallIcon, label: "SMALL"},
       {key: "medium", icon: icons.MediaMediumIcon, label: "MEDIUM"}
     ];
-    const defaultFeatured = "medium";
-    const blockProps = {plugin: {options: {featuredOptions, defaultFeatured}}};
+    const defaultDisplay = "medium";
+    const blockProps = {plugin: {options: {displayOptions, defaultDisplay}}};
 
     this.container = {
       updateData: sinon.spy()
@@ -51,27 +51,27 @@ describe("CommonBlock Component", function() {
     );
   });
 
-  it("renders default featured option when it is empty", function() {
+  it("renders default display option when it is empty", function() {
     this.component = this.renderComponent({});
     const dropdown = TestUtils.findRenderedComponentWithType(this.component, Dropdown);
     expect(dropdown.props.selected).to.equal("medium");
   });
 
-  it("renders selected featured option when it is present", function() {
-    const data = {featured: "small"};
+  it("renders selected display option when it is present", function() {
+    const data = {display: "small"};
     this.component = this.renderComponent(data);
 
     const dropdown = TestUtils.findRenderedComponentWithType(this.component, Dropdown);
     expect(dropdown.props.selected).to.equal("small");
   });
 
-  it("updates selected featured option on change", function () {
+  it("updates selected display option on change", function () {
     this.component = this.renderComponent({});
     const dropdown = TestUtils.findRenderedComponentWithType(this.component, Dropdown);
 
     dropdown.onChange("small");
 
-    expect(this.container.updateData).to.have.been.calledWith({featured: "small"});
+    expect(this.container.updateData).to.have.been.calledWith({display: "small"});
   });
 });
 
