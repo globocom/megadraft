b32c1e019824c46003c1042627c64455c7276ec0
Marcelo Jorge Vieira <metal@alucinados.com>
2016-12-12 21:15:04 -0200

    scripts/convert_svgo.sh: Remove xmlns and fixed fill-rule

--- scripts/convert_svgo.sh
+++ scripts/convert_svgo.sh
@@ -37,7 +37,7 @@ fi
 
 eval "node_modules/.bin/svgo -q $1";
 
-SVG_CONTENT=`cat $1`;
+SVG_CONTENT=`cat $1 | sed 's#xmlns="http://www.w3.org/2000/svg"##g' | sed 's#fill-rule#fillRule#g'`;
 create_js_file > $JS_OUTPUT;
 
 if [ ! -f "src/styles/icons/$SVG_FILE" ]; then
