1f184b47b04d4cb0715d11aac520ae3b1553b891
MayconDionisio <maycon.dionisio@gmail.com>
2017-02-15 16:08:38 -0200

    Fix icons modal styles and update modal (#130)

--- src/icons/video.js
+++ src/icons/video.js
@@ -10,7 +10,7 @@ import React from "react";
 export default class extends React.Component {
   render() {
     return (
-      <svg {...this.props} width="24" height="24">
+      <svg {...this.props} width="24" height="24" viewBox="0 0 24 24" >
         <path fill="currentColor" d="M10 9v6l5-3-5-3zm8.222-3H5.778C4.8 6 4 6.6 4 7.333v9.334C4 17.4 4.8 18 5.778 18h12.444C19.2 18 20 17.4 20 16.667V7.333C20 6.6 19.2 6 18.222 6z" fillRule="evenodd"/>
       </svg>
     );
