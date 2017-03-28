5f2ca26bf078745032217603d5c0589dd17cca34
Kevin Combriat <kevin.combriat@radiofrance.com>
2017-03-28 10:01:32 +0200

    Tests re-added

--- src/components/MegadraftEditor.js
+++ src/components/MegadraftEditor.js
@@ -34,6 +34,10 @@ const NO_RESET_STYLE_DEFAULT = ["ordered-list-item", "unordered-list-item"];
 
 
 export default class MegadraftEditor extends Component {
+  static defaultProps = {
+    actions: DEFAULT_ACTIONS,
+  }
+
   constructor(props) {
     super(props);
     this.state = {
@@ -335,7 +339,7 @@ export default class MegadraftEditor extends Component {
             editorState: this.props.editorState,
             readOnly: this.state.readOnly,
             onChange: this.onChange,
-            actions: this.props.actions || DEFAULT_ACTIONS,
+            actions: this.props.actions,
             entityInputs: this.entityInputs,
             shouldDisplayToolbarFn: this.props.shouldDisplayToolbarFn,
           })}
