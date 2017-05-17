2b15c2aea4f5f643fb5c25e48dfb9126529af192
Diego Manhães Pinheiro <diego.pinheiro@corp.globo.com>
2017-05-17 17:30:20 -0300

    onChange function from ModalPluginList toggles modal visibility when called.

--- tests/components/modalpluginlist_test.js
+++ tests/components/modalpluginlist_test.js
@@ -6,6 +6,7 @@
 
 import React, {Component} from "react";
 import chai from "chai";
+import sinon from "sinon";
 import {mount} from "enzyme";
 import cp from "utils-copy";
 
@@ -34,12 +35,23 @@ class ModalWithPlugins extends Component {
   }
 
   render() {
+    let onChange = this.onChange;
+    if (this.props.onChange) {
+      onChange = this.props.onChange;
+    }
+
+    let toggleModalVisibility = sinon.spy();
+    if (this.props.toggleModalVisibility) {
+      toggleModalVisibility = this.props.toggleModalVisibility;
+    }
+
     return (
       <div ref="editor">
         <ModalPluginList
           handleModal={this.handleModal}
           plugins={this.fakePlugins}
-          onChange={this.onChange}
+          onChange={onChange}
+          toggleModalVisibility={toggleModalVisibility}
           editorState={this.props.editorState} />
       </div>
     );
@@ -76,5 +88,27 @@ describe("Sidebar Modal Component", function() {
     const plugin = modal.find("VideoButton");
 
     expect(plugin.length).to.be.at.least(1);
+  });
+
+  it("should callback a function received when receives onChange call", function() {
+    const onChangeSpy = sinon.spy();
+    this.wrapper = mount(
+      <ModalWithPlugins onChange={onChangeSpy} />
+    );
+    const newEditorState = {};
+    const modal = this.wrapper.find(ModalPluginList);
+    modal.getNode().onChange(newEditorState);
+    expect(onChangeSpy).to.be.calledWith(newEditorState);
+  });
+
+  it("should toggle visibility when receives onChange call", function() {
+    const toggleModalVisibility = sinon.spy();
+    this.wrapper = mount(
+      <ModalWithPlugins toggleModalVisibility={toggleModalVisibility} />
+    );
+    const newEditorState = {};
+    const modal = this.wrapper.find(ModalPluginList);
+    modal.getNode().onChange(newEditorState);
+    expect(toggleModalVisibility).to.be.called;
   });
 });
