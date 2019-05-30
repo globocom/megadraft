bf9bb7ccc176d15b1b0418c3d10894786990e693
Ícaro Heimig <icaro.heimig@corp.globo.com>
2019-05-30 18:28:39 -0300

    remove unnecessary comments

--- website/components/megadrafttheme.js
+++ website/components/megadrafttheme.js
@@ -4,15 +4,11 @@
  * License: MIT
  */
 
-// import Spacing from "material-ui/styles/spacing";
-// import zIndex from "material-ui/styles/zIndex";
 import { grey } from "@material-ui/core/colors";
 
 import { createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
-  //   spacing: Spacing,
-  //   zIndex: zIndex,
   palette: {
     primary1Color: grey[900],
     accent1Color: grey[900]
