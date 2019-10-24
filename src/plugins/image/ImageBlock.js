bd9b050ae18658d2646d861984425d43fe290d15
Willian Dias Tamagi <will.tamagi@gmail.com>
2019-10-23 22:06:10 -0300

    Added selected state to ImageBlock component and added properties to CommonBlock for handle with events.

--- src/plugins/image/ImageBlock.js
+++ src/plugins/image/ImageBlock.js
@@ -24,6 +24,10 @@ export default class ImageBlock extends Component {
     this._handleCaptionChange = ::this._handleCaptionChange;
     this._handleRightsHolderChange = ::this._handleRightsHolderChange;
 
+    this.state = {
+      isSelected: false
+    };
+
     this.actions = [
       {
         key: "delete",
@@ -47,7 +51,14 @@ export default class ImageBlock extends Component {
     const readOnly = this.props.blockProps.getInitialReadOnly();
 
     return (
-      <CommonBlock {...this.props} actions={this.actions}>
+      <CommonBlock
+        {...this.props}
+        actions={this.actions}
+        style={this.state.isSelected ? ImageBlockStyle.selected : {}}
+        tabIndex={0}
+        onFocus={() => this.setState({ isSelected: true })}
+        onBlur={() => this.setState({ isSelected: false })}
+      >
         <BlockContent>
           <img style={ImageBlockStyle.image} src={this.props.data.src} alt="" />
         </BlockContent>
