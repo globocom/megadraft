293c38cfd5eb7549c2c2eb436149d7438b88a419
Mark Wilkinson <mwilkinson@ardensoftware.com>
2020-10-29 11:02:05 +0000

    Added humanReadable boolean to editorStateToJSON

--- src/utils.js
+++ src/utils.js
@@ -14,10 +14,10 @@ import {
 
 import defaultDecorator from "./decorators/defaultDecorator";
 
-export function editorStateToJSON(editorState) {
+export function editorStateToJSON(editorState, humanReadable = true) {
   if (editorState) {
     const content = editorState.getCurrentContent();
-    return JSON.stringify(convertToRaw(content), null, 2);
+    return JSON.stringify(convertToRaw(content), null, humanReadable ? 2 : 0);
   }
 }
 
