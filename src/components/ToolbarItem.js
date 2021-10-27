71ffdd73b9189cd1bb0af441f8b874f6a87256f4
Felipe Lopes <7880548+felops@users.noreply.github.com>
2021-10-27 00:28:49 -0300

    refactor: convert ToolbarItem to FC

--- src/components/ToolbarItem.js
+++ src/components/ToolbarItem.js
@@ -4,45 +4,39 @@
  * License: MIT
  */
 
-import React, { Component } from "react";
+import React from "react";
 import classNames from "classnames";
 
 import Separator from "./Separator";
 
-export default class ToolbarItem extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  toggleAction(action) {
-    if (action.toggle) {
-      action.toggle(!action.active);
+export default function ToolbarItem({ active, item, toggle }) {
+  const toggleAction = toggle => {
+    if (toggle) {
+      toggle(!active);
     }
+  };
+
+  const Icon = item.icon;
+
+  if (item.type == "separator") {
+    return <Separator />;
   }
 
-  render() {
-    const Icon = this.props.item.icon;
+  const className = classNames("toolbar__item", {
+    "toolbar__item--active": active
+  });
 
-    if (this.props.item.type == "separator") {
-      return <Separator />;
-    }
-
-    const className = classNames("toolbar__item", {
-      "toolbar__item--active": this.props.active
-    });
-
-    return (
-      <li className={className}>
-        <button
-          onClick={() => {
-            this.toggleAction(this.props);
-          }}
-          type="button"
-          className="toolbar__button"
-        >
-          <Icon />
-        </button>
-      </li>
-    );
-  }
+  return (
+    <li className={className}>
+      <button
+        onClick={() => {
+          toggleAction(toggle);
+        }}
+        type="button"
+        className="toolbar__button"
+      >
+        <Icon />
+      </button>
+    </li>
+  );
 }
