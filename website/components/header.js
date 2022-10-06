d2c492a0708dfe6c9aca35ebaf4bb5499285d0cd
Matheus Motta <70399238+mottamatheus@users.noreply.github.com>
2022-10-06 17:28:04 -0300

    feat: converts header and home to functional components (#393)
    Co-authored-by: Matheus Motta <matheus.cruz@g.globo>

--- website/components/header.js
+++ website/components/header.js
@@ -6,36 +6,30 @@
 
 import React from "react";
 
-export default class Header extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="page__header">
-        <div className="hero__container">
-          <div className="hero__logo">
-            <img
-              className="hero__logo-svg"
-              src="images/megadraft_white_version.svg"
-              alt="Logo Megadraft"
-            />
-          </div>
-          <h1 className="hero__title">
-            The <br />
-            most rock n’ roll <br />
-            text editor ever.
-          </h1>
-          <div className="hero__description">
-            Megadraft is a Rich Text editor built on top of
-            <br />
-            Facebook's draft.js featuring a nice default
-            <br />
-            base of plugins and extensibility.
-          </div>
+export default function Header() {
+  return (
+    <div className="page__header">
+      <div className="hero__container">
+        <div className="hero__logo">
+          <img
+            className="hero__logo-svg"
+            src="images/megadraft_white_version.svg"
+            alt="Logo Megadraft"
+          />
+        </div>
+        <h1 className="hero__title">
+          The <br />
+          most rock n’ roll <br />
+          text editor ever.
+        </h1>
+        <div className="hero__description">
+          Megadraft is a Rich Text editor built on top of
+          <br />
+          Facebook's draft.js featuring a nice default
+          <br />
+          base of plugins and extensibility.
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
--- website/components/home.js
+++ website/components/home.js
@@ -8,12 +8,10 @@ import React from "react";
 
 import Example from "../components/example";
 
-export default class Home extends React.Component {
-  render() {
-    return (
-      <div className="content home">
-        <Example activeContent={this.props.activeContent} />
-      </div>
-    );
-  }
+export default function Home({ activeContent }) {
+  return (
+    <div className="content home">
+      <Example activeContent={activeContent} />
+    </div>
+  );
 }
