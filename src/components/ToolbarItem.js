95aad17c0c15e3aa6d9267d5fe33c2cbd010558e
Taís Massaro <code@tais.dev>
2022-10-28 19:17:11 +0200

    refactor(components/toolbar-item): convert to functional component (#387)
    * docs(readme): add node version to dev steps
    
    * refactor(components/toolbar-item): convert to functional component

--- src/components/ToolbarItem.js
+++ src/components/ToolbarItem.js
@@ -4,45 +4,47 @@
  * License: MIT
  */
 
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import classNames from "classnames";
-
 import Separator from "./Separator";
 
-export default class ToolbarItem extends Component {
-  constructor(props) {
-    super(props);
-  }
+export default function ToolbarItem({ item, active, toggle }) {
+  const Icon = item.icon;
+  const className = classNames("toolbar__item", {
+    "toolbar__item--active": active
+  });
 
-  toggleAction(action) {
-    if (action.toggle) {
-      action.toggle(!action.active);
+  function toggleAction() {
+    if (toggle) {
+      toggle(!active);
     }
   }
 
-  render() {
-    const Icon = this.props.item.icon;
-
-    if (this.props.item.type == "separator") {
-      return <Separator />;
-    }
-
-    const className = classNames("toolbar__item", {
-      "toolbar__item--active": this.props.active
-    });
-
-    return (
-      <li className={className}>
-        <button
-          onClick={() => {
-            this.toggleAction(this.props);
-          }}
-          type="button"
-          className="toolbar__button"
-        >
-          <Icon />
-        </button>
-      </li>
-    );
+  if (item.type == "separator") {
+    return <Separator />;
   }
+
+  return (
+    <li className={className}>
+      <button
+        onClick={() => toggleAction()}
+        type="button"
+        className="toolbar__button"
+      >
+        <Icon />
+      </button>
+    </li>
+  );
 }
+
+ToolbarItem.propTypes = {
+  item: PropTypes.shape({
+    type: PropTypes.string.isRequired,
+    label: PropTypes.string,
+    icon: PropTypes.func.isRequired,
+    style: PropTypes.string
+  }),
+  active: PropTypes.bool,
+  toggle: PropTypes.func
+};
